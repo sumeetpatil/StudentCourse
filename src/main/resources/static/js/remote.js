02777f975d73ddf644541108e3ce69b2e8f281a5
sumeet patil <sumeetpatil@sumeets-MacBook-Pro.local>
2019-06-30 23:57:57 +0530

    Fix host name

--- src/main/resources/static/js/remote.js
+++ src/main/resources/static/js/remote.js
@@ -1,28 +1,29 @@
 var remote = {
-		get : function(href) {
-			var retData = "";
-			$.ajax({
-				url : href,
-				method : "GET",
-				async:false,
-				success : function(data) {
-					retData = data;
-				},
-				error : function(data) {
-					console.log("error:" + data);
-					retData = "error";
-				}
-			});
-
-			return retData;
-		},
-		student : {
-			get : function(id) {
-				var url = remote.getHost() + "students/" + id;
-				return remote.get(url);
+	get : function(href) {
+		href = href.replace("http://localhost:8080/", remote.getHost());
+		var retData = "";
+		$.ajax({
+			url : href,
+			method : "GET",
+			async : false,
+			success : function(data) {
+				retData = data;
+			},
+			error : function(data) {
+				console.log("error:" + data);
+				retData = "error";
 			}
-		},
-		getHost : function() {
-			return "http://localhost:8080/";
+		});
+
+		return retData;
+	},
+	student : {
+		get : function(id) {
+			var url = remote.getHost() + "students/" + id;
+			return remote.get(url);
 		}
+	},
+	getHost : function() {
+		return "http://localhost:8080/";
+	}
 };
